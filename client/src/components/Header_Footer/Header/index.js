1547c807840d027054d46546eece8fc61678760b
alkas <rdxtbl@gmail.com>
2020-02-26 15:41:49 +0200

    reponsive ip

--- client/src/components/Header_Footer/Header/index.js
+++ client/src/components/Header_Footer/Header/index.js
@@ -2,17 +2,18 @@
  * Header for User dashboard
  */
 import React, { Component } from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+
+import { Link } from 'react-router-dom';
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import Dropdown from 'react-dropdown';
+import Popover from '@material-ui/core/Popover';
 import 'react-dropdown/style.css';
 
 import { FaGlobe } from 'react-icons/fa';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 import logo from '../../../Resources/logo.webp';
 
@@ -26,22 +27,55 @@ class Header extends Component {
         menu: {},
         options: ["EN", "DE"],
         ddValue: '',
-        locale: 'en'
+        locale: 'en',
+        ddMenuOpen: false,
+        ddBurgerOpen: false,
+        anchorElement: null,
+        anchorElementDD: null,
+        windowWidth: 0
     }
 
     componentDidMount(){
+        window.addEventListener('resize', this.handleWindowResize);
         // console.log('HANDLE ON SELECT: MENU=', MENU)
-        this.setState({menu: MENU});
+        this.setState({
+            menu: MENU
+            // windowWidth: window.size
+        });
 
         this.handleLocale('first loading');
+
     }
 
-    handleOnSelect = e => {
-        console.log('HANDLE ON SELECT: e=', e.value.toLowerCase())
-        this.handleLocale(e.value.toLowerCase());
-        document.dispatchEvent(this.languageChanged);
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleWindowResize);
+      }
+
+    handleBurgerMenuOpen = e => {
+        this.setState({
+            ddBurgerOpen: true,
+            anchorElement: e.currentTarget
+        })
     }
 
+    handleBurgerMenuClose = () => {
+        this.setState({
+            ddBurgerOpen: false
+        })
+    }
+
+    handleDDMenuClose = () => {
+        this.setState({
+           ddMenuOpen:false
+        });
+    }
+
+    handleDDMenuOpen = e => {
+        this.setState({
+            ddMenuOpen: true,
+            anchorElementDD: e.currentTarget
+        });
+    }
 
     handleLocale = e => {
 
@@ -94,34 +128,114 @@ class Header extends Component {
         this.setState({locale, ddValue: locale.toUpperCase()});
     }
 
+    handleOnSelect = e => {
+        console.log('HANDLE ON SELECT: e=', e.value.toLowerCase())
+        this.handleLocale(e.value.toLowerCase());
+        document.dispatchEvent(this.languageChanged);
+        this.handleWindowResize()
+    }
+
+    handleWindowResize = () => {
+        // console.log('HANDLE WINDOW RESIZE: size=', window.innerWidth);
+        this.setState({windowWidth: window.innerWidth})
+    }
+
     render() {
         return (
             <div className="header_container"  id='headerElement'>
-                <Navbar bg="transparent" expand="md">
-                    <Navbar.Brand href="#home"><img src={logo} alt='Pro Sport Coin'/></Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mr-auto">
-                        {/* <Nav.Link href="#home">{this.state.menu.home[this.state.locale]}</Nav.Link> */}
-                        <Nav.Link href="/">{MENU.home[this.state.locale]}</Nav.Link>
-                        <Nav.Link href="#link">{MENU.thePSC[this.state.locale]}</Nav.Link>
-                        <Nav.Link href="#link">{MENU.joinPSC[this.state.locale]}</Nav.Link>
-                        <Nav.Link href="#link">{MENU.purchaseGuide[this.state.locale]}</Nav.Link>
-                        <Nav.Link href="#link">{MENU.buy[this.state.locale]}</Nav.Link>
-                        <Nav.Link href="#link">{MENU.theFootbalMarket[this.state.locale]}</Nav.Link>
-                        <Nav.Link href="/news">{MENU.news[this.state.locale]}</Nav.Link>
-                        <NavDropdown title={MENU.more[this.state.locale]} id="collasible-nav-dropdown" >
-                            <NavDropdown.Item href="#action/3.2">{MENU.whoAreWe[this.state.locale]}</NavDropdown.Item>
-                            <NavDropdown.Item href="/contact">{MENU.contactUs[this.state.locale]}</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">{MENU.disclaimer[this.state.locale]}</NavDropdown.Item>
-                        </NavDropdown>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
+                
+                <div className='logo'><Link to='/'><img src={logo} alt='Pro Sport Coin Logo'/></Link></div>
+                <div className='menu_wrapper'>
+                   {
+                       this.state.windowWidth > 900 ? 
+                       <div className='menu_container'>
+                           {console.log('HANDLE WINDOW RESIZE: wiindow > 600')}
+                           <Link to='/'>{MENU.home[this.state.locale]}</Link>
+                           <Link to='/'>{MENU.thePSC[this.state.locale]}</Link>
+                           <Link to='/'>{MENU.joinPSC[this.state.locale]}</Link>
+                           <Link to='/'>{MENU.purchaseGuide[this.state.locale]}</Link>
+                           <Link to='/'>{MENU.buy[this.state.locale]}</Link>
+                           <Link to='/'>{MENU.theFootbalMarket[this.state.locale]}</Link>
+                           <Link to='/news'>{MENU.news[this.state.locale]}</Link>
+                           
+                           <div className='ddHeader' onClick={e => this.handleDDMenuOpen(e)}>
+                               {MENU.more[this.state.locale]}</div>
+                               {this.state.ddMenuOpen && (
+                               <Popover
+                                   anchorEl={this.state.anchorElementDD}
+                                   open={this.state.ddMenuOpen}
+                                   onClose={this.handleDDMenuClose}
+                                   anchorOrigin={{
+                                       vertical: 'bottom',
+                                       horizontal: 'center',
+                                   }}
+                                   transformOrigin={{
+                                       vertical: 'top',
+                                       horizontal: 'center',
+                                   }}
+                                   style={{
+                                       width: '20rem',
+                                       height: '10rem'
+                                   }}
+                               >
+                                   <div className="ddMenuContainer" >
+                                       <ul>
+                                           <li className ='menu_li_item'><Link to='/'>{MENU.whoAreWe[this.state.locale]}</Link></li>
+                                           <li className ='menu_li_item'><Link to='/contact'>{MENU.contactUs[this.state.locale]}</Link></li>
+                                           <hr />
+                                           <li className ='menu_li_item'><Link to='/contact'>{MENU.disclaimer[this.state.locale]}</Link></li>
+                                       </ul>
+                                   </div>
+                               </Popover>
+                               )}                        
+                           
+           
+                       </div>
+                       
+                   :
+                       <div className='menu_container_burger'>
+                           {console.log('HANDLE WINDOW RESIZE: wiindow < 600')}
+                           <button type="button" className="button" onClick={e => this.handleBurgerMenuOpen(e)}><GiHamburgerMenu /></button>
+                           {this.state.ddBurgerOpen && (
+                           <Popover
+                               anchorEl={this.state.anchorElement}
+                               open={this.state.ddBurgerOpen}
+                               onClose={this.handleBurgerMenuClose}
+                               anchorOrigin={{
+                                   vertical: 'bottom',
+                                   horizontal: 'center',
+                               }}
+                               transformOrigin={{
+                                   vertical: 'top',
+                                   horizontal: 'center',
+                               }}
+                           >
+                               <div className="ddBurgerMenuContainer" >
+                                   <ul>
+           
+                                       <li className ='menu_li_item'><Link to='/'>{MENU.home[this.state.locale]}</Link></li>
+                                       <li className ='menu_li_item'><Link to='/'>{MENU.thePSC[this.state.locale]}</Link></li>
+                                       <li className ='menu_li_item'><Link to='/'>{MENU.joinPSC[this.state.locale]}</Link></li>
+                                       <li className ='menu_li_item'><Link to='/'>{MENU.purchaseGuide[this.state.locale]}</Link></li>
+                                       <li className ='menu_li_item'><Link to='/'>{MENU.buy[this.state.locale]}</Link></li>
+                                       <li className ='menu_li_item'><Link to='/'>{MENU.theFootbalMarket[this.state.locale]}</Link></li>
+                                       <li className ='menu_li_item'><Link to='/news'>{MENU.news[this.state.locale]}</Link></li>
+           
+                                       <hr />
+                                       <li className ='menu_li_item'><Link to='/'>{MENU.whoAreWe[this.state.locale]}</Link></li>
+                                       <li className ='menu_li_item'><Link to='/contact'>{MENU.contactUs[this.state.locale]}</Link></li>
+                                       <li className ='menu_li_item'><Link to='/contact'>{MENU.disclaimer[this.state.locale]}</Link></li>
+                                   </ul>
+                               </div>
+                           </Popover>
+                           )}     
+                       </div>
+                   }
+                
                 <div className='language_selector_container' >
                     <div className="lang_icon"><FaGlobe/></div>
                     <Dropdown options={this.state.options} onChange={this.handleOnSelect} value={this.state.ddValue} placeholder="Select an option" />
+                </div>
                 </div>
                 <ToastContainer autoClose={3000} />
             </div>
